88aa93d3b82f8b3e979258d89f0d75982572e46d
chaitanyapvk1993 <1993chaitanyapvk@gmail.com>
2021-05-24 01:30:44 +0530

    Update index.js

--- Single-Component-Debug-React-BoilerPlate-main/src/index.js
+++ Single-Component-Debug-React-BoilerPlate-main/src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-ReactDOM.render
-(
-    
-        <p>I am learning React. My life is getting better.</p>
-         document.getElementById("root")
-    
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
 
